bf71c8564d3d220241265259d1f2cbe94f3fd2a7
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-19 23:30:29 +0300

    createAsyncThunk

--- src/redux/todoList/operations.js
+++ src/redux/todoList/operations.js
@@ -1,37 +1,56 @@
 import axios from "axios";
 import { addTodo, deleteTodo, isError, isLoading, refetchDataSuccess } from "./slice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = 'https://6649d3994032b1331beeebaf.mockapi.io/'
 
-export const fetchTodosThunk = () => {
-    return async dispatch => {
-        try {
-            dispatch(isLoading(true))
-            dispatch(isError(false))
-            const { data } = await axios.get('/Todos')
-            dispatch(refetchDataSuccess(data))
-        } catch (error) {
-            console.log(error)
-            dispatch(isError(true))
-        } finally {
-            dispatch(isLoading(false))
-        }
+export const fetchTodosThunk = createAsyncThunk('Todos/FetchAll', async (_, thunkApi) => {
+    try {
+        const { data } = await axios.get('Todos')
+        return data
+    } catch (error) {
+        return thunkApi.rejectWithValue(error.message)
     }
-}
+})
 
-export const deleteTodoThunk = (id) => {
-    return async dispatch => {
-        try {
-            dispatch(isLoading(true))
-            await axios.delete(`/Todos/${id}`)
-            dispatch(deleteTodo(id))
-        } catch (error) {
-            dispatch(isError(true))
-        } finally {
-            dispatch(isLoading(false))
-        }
+export const deleteTodoThunk = createAsyncThunk('Todos/Delete', async (id, thunkApi) => {
+    try {
+        const { data } = await axios.delete(`Todos/${id}`)
+        return data.id
+    } catch (error) {
+        return thunkApi.rejectWithValue(error.message)
     }
-}
+})
+
+// export const fetchTodosThunk = () => {
+//     return async dispatch => {
+//         try {
+//             dispatch(isLoading(true))
+//             dispatch(isError(false))
+//             const { data } = await axios.get('/Todos')
+//             dispatch(refetchDataSuccess(data))
+//         } catch (error) {
+//             console.log(error)
+//             dispatch(isError(true))
+//         } finally {
+//             dispatch(isLoading(false))
+//         }
+//     }
+// }
+
+// export const deleteTodoThunk = (id) => {
+//     return async dispatch => {
+//         try {
+//             dispatch(isLoading(true))
+//             await axios.delete(`/Todos/${id}`)
+//             dispatch(deleteTodo(id))
+//         } catch (error) {
+//             dispatch(isError(true))
+//         } finally {
+//             dispatch(isLoading(false))
+//         }
+//     }
+// }
 
 export const addTodoThunk = body => {
     return async dispatch => {
--- src/redux/todoList/slice.js
+++ src/redux/todoList/slice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit"
+import { deleteTodoThunk, fetchTodosThunk } from "./operations"
 // Створюємо початковий стан
 const initialState = {
     todos: [],
@@ -22,55 +23,73 @@ const slice = createSlice({
     },
 
     // Створюємо редюсери(екшени)
-    reducers: {
-        deleteTodo: (state, action) => {
-            state.todos = state.todos.filter(todo => todo.id !== action.payload)
-        },
-        toggleTodo: (state, action) => {
-            const item = state.todos.find(todo => todo.id === action.payload)
-            if (item) {
-                item.completed = !item.completed
-            }
-        },
-        likeTodo: (state, action) => {
-            const item = state.todos.find(item => item.id === action.payload)
-            if (item) {
-                item.liked = !item.liked
-            }
-        },
-        // Підготовка пейлоада відбувається таким чином
-        // Спочатку викликається prepare
-        // ПОтім викликається reducer
-        addTodo: {
-            prepare: todo => {
-                return {
-                    payload: {
-                        ...todo,
-                        id: nanoid(),
-                        completed: false,
-                        liked: false
-                    }
-                }
-            },
-            reducer: (state, action) => {
-                state.todos.push(action.payload)
-            }
-        },
-        refetchDataSuccess: (state, action) => {
-            state.todos = action.payload
-            state.isLoading = false
-        },
-        isError: (state, action) => {
-            state.isError = action.payload
-        },
-        isLoading: (state, action) => {
-            state.isLoading = action.payload
-        },
+    // reducers: {
+    //     deleteTodo: (state, action) => {
+    //         state.todos = state.todos.filter(todo => todo.id !== action.payload)
+    //     },
+    //     toggleTodo: (state, action) => {
+    //         const item = state.todos.find(todo => todo.id === action.payload)
+    //         if (item) {
+    //             item.completed = !item.completed
+    //         }
+    //     },
+    //     likeTodo: (state, action) => {
+    //         const item = state.todos.find(item => item.id === action.payload)
+    //         if (item) {
+    //             item.liked = !item.liked
+    //         }
+    //     },
+    //     // Підготовка пейлоада відбувається таким чином
+    //     // Спочатку викликається prepare
+    //     // ПОтім викликається reducer
+    //     addTodo: {
+    //         prepare: todo => {
+    //             return {
+    //                 payload: {
+    //                     ...todo,
+    //                     id: nanoid(),
+    //                     completed: false,
+    //                     liked: false
+    //                 }
+    //             }
+    //         },
+    //         reducer: (state, action) => {
+    //             state.todos.push(action.payload)
+    //         }
+    //     },
+    //     refetchDataSuccess: (state, action) => {
+    //         state.todos = action.payload
+    //         state.isLoading = false
+    //     },
+    //     isError: (state, action) => {
+    //         state.isError = action.payload
+    //     },
+    //     isLoading: (state, action) => {
+    //         state.isLoading = action.payload
+    //     },
+    // },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchTodosThunk.pending, state => {
+                state.isLoading = true
+            })
+            .addCase(fetchTodosThunk.fulfilled, (state, { payload }) => {
+                state.todos = payload
+                state.isLoading = false
+            })
+            .addCase(deleteTodoThunk.fulfilled, (state, { payload }) => {
+                state.todos = state.todos.filter(item => item.id !== payload)
+                state.isLoading = false
+            })
+            .addCase(deleteTodoThunk.pending, state => {
+                state.isLoading = true
+            })
     }
 })
 // Експорт редюсера (його стан, логіка)
 export const todoReducer = slice.reducer
 // Експорт екшенів - того, що написано в реюсерах
-export const { deleteTodo, toggleTodo, likeTodo, addTodo, refetchDataSuccess, isError, isLoading } = slice.actions;
+// export const { deleteTodo, toggleTodo, likeTodo, addTodo, refetchDataSuccess, isError, isLoading } = slice.actions;
+
 // Експорт селекторів
 export const { selectTodos, selectIsLoading, selectIsError } = slice.selectors
